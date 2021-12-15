b9c031ca70c77dc845ec5f4b99ac78a1b2e463ce
HalilKakut <halil.kakut@hotmail.com>
2021-12-15 12:50:42 +0300

    https://github.com/halilkakut/PatikaDevAlgoritmaOdev1.git

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace WordOperation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lütfen Bir Cümle giriniz");
+
+            string sentence = Console.ReadLine();
+
+            string[] kelimeler = sentence.Split(' ');
+            Console.WriteLine("Kelime Sayısı :" + kelimeler.Length);
+            foreach(string kelime in kelimeler)
+            {
+                Console.WriteLine(kelime);
+            }
+
+            string liste =  "ABCÇDEFGĞHİIJKLMNOÖPRSŞTUÜVYZ"
+                           +"abcçdefgğhiıjklmnoöprsştuüvyz";
+
+            int sayac = 0;
+
+            for (int i =0; i<sentence.Length;i++)
+            {
+                if(liste.Contains(sentence[i])){
+                    sayac++;
+                }
+            }
+
+            Console.WriteLine("Girilen Cümlede {0} tane harf vardır",sayac);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Patika dev c# 101
+## Algortima Odev 1
